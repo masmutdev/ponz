31549ea564a7278a42de84b151802f2f07ad8c4c
Masmut Dev <masmutofficial@gmail.com>
2025-06-07 19:58:01 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url)
+
     try {
       return await env.ASSETS.fetch(request)
     } catch (e) {
-      // Jika bukan permintaan untuk file statis, arahkan ke index.html
       if (!url.pathname.match(/\.\w+$/)) {
         return await env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request))
       }
+
       return new Response('Not found', { status: 404 })
     }
   },
