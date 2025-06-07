12788225157ebd56d1b5d74d263f8a184c528301
Masmut Dev <masmutofficial@gmail.com>
2025-06-07 19:04:52 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'dist', // folder output untuk Cloudflare Pages
+    emptyOutDir: true,
+  },
+  base: '/', // penting untuk Cloudflare (jangan './', gunakan '/' jika host di root domain)
 })
